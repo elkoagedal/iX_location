4729f3637c4a90ece2d760a10f4ab5b53e6d46d7
Emily Koagedal <elkoagedal@gmail.com>
2017-06-05 09:50:38 +0200

    Initial Commit

---
+++ iX_Location/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iX_Location
+//
+//  Created by Emily Koagedal on 6/5/17.
+//  Copyright © 2017 Emily Koagedal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
